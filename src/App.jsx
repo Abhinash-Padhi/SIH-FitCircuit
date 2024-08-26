f3775c1cd2a7d22219703de32129ca268945f014
Abhinash-Padhi <abhinashpadhi79@gmail.com>
2024-08-26 23:19:31 +0530

    Added initial setup of sign up,log in and profile

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Signup from './SignUp';
 import Signin from './SignIn';
 import Profile from './Profile';
 import CreateProfile from './CreateProfile';
+import Home from './Home'
+import About from './About'
+import Socials from './Socials'
+import Contact from './Contact'
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/socials" element={<Socials />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/createprofile" element={<CreateProfile />} />
       </Routes>
