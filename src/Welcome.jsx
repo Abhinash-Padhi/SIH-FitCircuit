a5f2cd17310df0c0ee4da0b36479217161ad35ad
Subhra-Sarojini-Sahoo <152807439+Subhra-Sarojini-Sahoo@users.noreply.github.com>
2024-08-28 21:02:12 +0530

    Update Welcome.jsx

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,1 +1,25 @@
+import React from 'react';
+import './Welcome.css'; 
 
+function FitnessApp() {
+  return (
+    <div className="container">
+      <div className="image-container">
+        <img id="fitness-image" src="potrait.jpg" alt="Fitness" />
+      </div>
+      <div className="text-overlay">
+        <h1>Welcome to FitCircuit!</h1>
+        <p>Empowering you to achieve your fitness goals every step of the way.</p>
+        <ul>
+          <li>Track and Monitor Progress</li>
+          <li>Enhance Nutritional Habits</li>
+          <li>Improve Sleep Quality</li>
+          <li>Increase Physical Activity</li>
+        </ul>
+        <button className="get-started-button">Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default FitnessApp;
