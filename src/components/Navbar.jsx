ef218f1bcc960cf0b209d0283f9445edb1c592c9
Subhranshu-Dhal <153459702+Subhranshu-Dhal@users.noreply.github.com>
2024-08-29 22:54:39 +0530

    Updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,6 @@ const Navbar = () => {
                         <li><Link className={location.pathname === '/Home' ? 'active' : ''} to="/Home"><FontAwesomeIcon icon={faHome} /> Home</Link></li>
                         <li><Link className={location.pathname === '/About' ? 'active' : ''} to="/About"><FontAwesomeIcon icon={faInfoCircle} /> About Us</Link></li>
                         <li><Link className={location.pathname === '/Profile' ? 'active' : ''} to="/Profile"><FontAwesomeIcon icon={faUser} /> Profile</Link></li>
-                        <li><Link className={location.pathname === '/Scocials' ? 'active' : ''} to="/Tour"><FontAwesomeIcon icon={faTrophy} /> Socials</Link></li>
                         <li><Link className={location.pathname === '/Contact' ? 'active' : ''} to="/Contact"><FontAwesomeIcon icon={faEnvelope} /> Contact Us</Link></li>
                         <li><Link className={location.pathname === '/Log out' ? 'active' : ''} to="/SignIn"><button><FontAwesomeIcon icon={faSignOutAlt} /> Log out</button></Link></li>
                         <a href="#" id="close" onClick={handleCloseClick}><FontAwesomeIcon icon={faTimes} /></a>
